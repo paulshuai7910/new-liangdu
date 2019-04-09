1bd3366956b85d6160b60ef48342711c81994a67
Paul <2900717209@qq.com>
2019-04-09 22:23:54 +0800

    完善表单页面

--- pages/vendorenter/vendorenter.js
+++ pages/vendorenter/vendorenter.js
@@ -20,6 +20,9 @@ Page({
     isCheckedTime:false,
     isCheckedAgree:false
   },
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
   onLoad: function(options) {
     this.GetPlatformCategory();
   },
